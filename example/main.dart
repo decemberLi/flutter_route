bd1f54d4703fb8c8f68ea404782aed40e480dd94
dec <252539361@qq.com>
2021-05-21 09:28:11 +0800

    update example

--- example/main.dart
+++ example/main.dart
@@ -1,6 +1,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:yyy_route_annotation/route_serializable.dart';
+import 'main.all.dart';
 
 @RoutePage()
 class TestPge extends StatelessWidget {
@@ -12,5 +13,15 @@ class TestPge extends StatelessWidget {
 
 @RouteMain()
 void main(){
-  runApp(app)
+  runApp(Home());
+}
+
+class Home extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "RouteTest",
+      routes: allRoutes,
+    );
+  }
 }
